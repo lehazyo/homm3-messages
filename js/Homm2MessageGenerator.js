721dcd530bd5536b18245561a711abadc7f130cf
Алексей Сергиенко <lehazyo@gmail.com>
2021-03-28 00:53:33 +0300

    HoMM2 diacritics chars related bug fixes

--- js/Homm2MessageGenerator.js
+++ js/Homm2MessageGenerator.js
@@ -466,7 +466,7 @@ Homm2MessageGenerator.letter_container_width = 19;
 Homm2MessageGenerator.line_height = 16;
 Homm2MessageGenerator.letters_offset_x = 1;
 Homm2MessageGenerator.letters_offset_y = 313;
-Homm2MessageGenerator.letters_shadow_offset_y = 483;
+Homm2MessageGenerator.letters_shadow_offset_y = 499;
 Homm2MessageGenerator.letter_spacing = 2;
 
 Homm2MessageGenerator.sections_info = {
@@ -1389,185 +1389,185 @@ Homm2MessageGenerator.letters = {
   },
   "ą": {
     "x": 0,
-    "y": 9,
+    "y": 10,
     "width": 9
   },
   "ć": {
     "x": 1,
-    "y": 9,
+    "y": 10,
     "width": 8
   },
   "ę": {
     "x": 2,
-    "y": 9,
+    "y": 10,
     "width": 8
   },
   "ł": {
     "x": 3,
-    "y": 9,
+    "y": 10,
     "width": 7
   },
   "ń": {
     "x": 4,
-    "y": 9,
+    "y": 10,
     "width": 9
   },
   "ó": {
     "x": 5,
-    "y": 9,
+    "y": 10,
     "width": 8
   },
   "ś": {
     "x": 6,
-    "y": 9,
+    "y": 10,
     "width": 7
   },
   "ź": {
     "x": 7,
-    "y": 9,
+    "y": 10,
     "width": 8
   },
   "ż": {
     "x": 8,
-    "y": 9,
+    "y": 10,
     "width": 8
   },
   "Ą": {
     "x": 9,
-    "y": 9,
+    "y": 10,
     "width": 14
   },
   "Ć": {
     "x": 10,
-    "y": 9,
+    "y": 10,
     "width": 12
   },
   "Ę": {
     "x": 11,
-    "y": 9,
+    "y": 10,
     "width": 10,
     "extraHeightDown": 1
   },
   "Ł": {
     "x": 12,
-    "y": 9,
+    "y": 10,
     "width": 10
   },
   "Ń": {
     "x": 13,
-    "y": 9,
+    "y": 10,
     "width": 14,
     "extraHeightUp": 4
   },
   "Ó": {
     "x": 14,
-    "y": 9,
+    "y": 10,
     "width": 12,
     "extraHeightUp": 4
   },
   "Ś": {
     "x": 15,
-    "y": 9,
+    "y": 10,
     "width": 9,
     "extraHeightUp": 4
   },
   "Ź": {
     "x": 17,
-    "y": 9,
+    "y": 10,
     "width": 10,
     "extraHeightUp": 4
   },
   "Ż": {
     "x": 18,
-    "y": 9,
+    "y": 10,
     "width": 10,
     "extraHeightUp": 4
   },
   "á": {
     "x": 16,
-    "y": 9,
+    "y": 10,
     "width": 9
   },
   "é": {
     "x": 19,
-    "y": 9,
+    "y": 10,
     "width": 8
   },
   "í": {
     "x": 20,
-    "y": 9,
+    "y": 10,
     "width": 4
   },
   "ö": {
     "x": 21,
-    "y": 9,
+    "y": 10,
     "width": 8
   },
   "ő": {
     "x": 22,
-    "y": 9,
+    "y": 10,
     "width": 8
   },
   "ú": {
     "x": 23,
-    "y": 9,
+    "y": 10,
     "width": 9
   },
   "ű": {
     "x": 24,
-    "y": 9,
+    "y": 10,
     "width": 9
   },
   "ü": {
     "x": 25,
-    "y": 9,
+    "y": 10,
     "width": 9
   },
   "Á": {
     "x": 26,
-    "y": 9,
+    "y": 10,
     "width": 14,
     "extraHeightUp": 4
   },
   "É": {
     "x": 27,
-    "y": 9,
+    "y": 10,
     "width": 10,
     "extraHeightUp": 4
   },
   "Í": {
     "x": 28,
-    "y": 9,
+    "y": 10,
     "width": 6,
     "extraHeightUp": 4
   },
   "Ö": {
     "x": 29,
-    "y": 9,
+    "y": 10,
     "width": 12,
     "extraHeightUp": 4
   },
   "Ő": {
     "x": 30,
-    "y": 9,
+    "y": 10,
     "width": 12,
     "extraHeightUp": 4
   },
   "Ú": {
     "x": 31,
-    "y": 9,
+    "y": 10,
     "width": 11,
     "extraHeightUp": 4
   },
   "Ű": {
     "x": 32,
-    "y": 9,
+    "y": 10,
     "width": 11,
     "extraHeightUp": 4
   },
   "Ü": {
     "x": 33,
-    "y": 9,
+    "y": 10,
     "width": 11,
     "extraHeightUp": 4
   },
